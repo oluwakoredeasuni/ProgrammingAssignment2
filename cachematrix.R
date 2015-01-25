af5ff5a38f41a1de3ace4af7fd85c98f0768cb59
oluwakoredeasuni <oluwakoredeasuni@gmail.com>
2015-01-25 08:55:25 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates spencil matrix capable of caching it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
